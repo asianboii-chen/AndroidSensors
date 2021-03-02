1dd62c0bfc5b00a717b05182671f35f09d1c63f5
Qianlang Chen <qianlangchen@gmail.com>
2021-03-02 14:57:24 -0700

    Basic Sensors - Created a basic Tizen detector for heart rate and acceleration

---
+++ TizenSensor/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TizenSensor.lib;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TizenSensor
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+			SensorPermissionGetter.GetPermission(Init);
+		}
+
+		protected Sensor sensor;
+
+		protected void Init(bool hasPermission)
+		{
+			if (!hasPermission)
+			{
+				Application.Current.Quit();
+				return;
+			}
+
+			sensor = new Sensor(OnSensorUpdate, 1000);
+			sensor.Start();
+		}
+
+		protected void OnSensorUpdate(int heartRate, float accelX, float accelY, float accelZ)
+		{
+			dataLabel.Text = $"{heartRate}, {accelX:0.0}, {accelY:0.0}, {accelZ:0.0}";
+		}
+	}
+}
---
+++ TizenSensor/lib/Sensor.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Timers;
+
+using Tizen.Sensor;
+
+namespace TizenSensor.lib
+{
+	public class Sensor
+	{
+		public Sensor(Action<int, float, float, float> updateCallback, uint reportInterval = 100)
+		{
+			UpdateCallback = updateCallback;
+
+			heartRateMonitor = new HeartRateMonitor
+			{
+				Interval = reportInterval,
+				PausePolicy = SensorPausePolicy.None
+			};
+			accelerometer = new Accelerometer
+			{
+				Interval = reportInterval,
+				PausePolicy = SensorPausePolicy.None
+			};
+			timer = new Timer
+			{
+				Interval = reportInterval,
+				AutoReset = true
+			};
+			timer.Elapsed += Timer_Elapsed;
+		}
+
+		protected HeartRateMonitor heartRateMonitor;
+
+		protected Accelerometer accelerometer;
+
+		protected Timer timer;
+
+		public Action<int, float, float, float> UpdateCallback { get; set; }
+
+		public void Start()
+		{
+			heartRateMonitor.Start();
+			accelerometer.Start();
+			timer.Start();
+		}
+
+		public void Stop()
+		{
+			heartRateMonitor.Stop();
+			accelerometer.Stop();
+			timer.Stop();
+		}
+
+		private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+		{
+			UpdateCallback?.Invoke(heartRateMonitor.HeartRate,
+				accelerometer.X,
+				accelerometer.Y,
+				accelerometer.Z
+			);
+		}
+	}
+}
